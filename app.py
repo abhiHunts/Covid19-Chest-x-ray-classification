dcaef90b1824e6ec72c8b6b7fa507c6a2828ca26
abhiHunts <abhishek626pawar@gmail.com>
2022-12-22 16:29:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 import tensorflow as tf
 st.set_option('deprecation.showfileUploaderEncoding', False)
 from PIL import Image
@@ -26,15 +26,24 @@ def load_and_predict(img, model):
     img = tf.expand_dims(img, axis=0)
     img = img/255.
     return model.predict(img)
+col1, col2  = st.columns(2)
 
-if file is None:
-    st.text('Please Upload and Image file')
-else:
-    img = Image.open(file)
-    st.image(img, use_column_width=True)
-    predictions = load_and_predict(file.getvalue(), model)
-    class_names = ['Normal', 'Viral Pneumonia', 'Covid']
-    pred_class = class_names[predictions.argmax()]
-    
-    st.success('This x-ray is of type : '+pred_class)
+with col1:
+    if file is None:
+        st.text('Please Upload and Image file')
+    else:        
+        img = Image.open(file)
+        st.image(img, use_column_width=True)
+        print(type(file.getvalue()))
+        predictions = load_and_predict(file.getvalue(), model)
+        class_names = ['Normal', 'Viral Pneumonia', 'Covid']
+        print(np.sort(predictions)[::-1])
+        pred_class = class_names[predictions.argmax()]
+
+        with col2:
+            if pred_class == 'Normal':
+                st.success('This x-ray is of type : '+pred_class)
+            else:
+                st.error('This x-ray is of type : '+pred_class)
+
     
